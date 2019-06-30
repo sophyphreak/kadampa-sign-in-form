596f22920c9272da45a74fa81406a68321334084
Andrew Horn <arhorn@smcm.edu>
2019-06-30 01:22:52 -0500

    changed heading, title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Kadampa Meditation Sign In`,
+    title: `Hi! Please sign in :-)`,
     description: `Houston Kadampa meditation group sign in sheet`,
     author: `Andrew Horn`
   },
