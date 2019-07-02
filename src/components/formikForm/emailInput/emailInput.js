58bd31ebf126b7cb2fb5bc3f61089f7a9a511884
Andrew Horn <arhorn@smcm.edu>
2019-07-01 19:31:53 -0500

    changed email input to text input

--- src/components/formikForm/emailInput/emailInput.js
+++ src/components/formikForm/emailInput/emailInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BasicInput from '../basicInput/basicInput';
 
 const EmailInput = props => (
-  <BasicInput componentName="email" label="Email" type="email" {...props} />
+  <BasicInput componentName="email" label="Email" type="text" {...props} />
 );
 
 export default EmailInput;
