1cede04c7e548ff3e1550720e064ea8cb871b69b
Andrew Horn <arhorn@smcm.edu>
2019-07-22 01:02:17 -0500

    added download xlsx button to TablePage

--- src/pages/table.js
+++ src/pages/table.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import queryString from 'query-string';
-import { Button, Table } from 'reactstrap';
+import XLSX from 'xlsx';
+import moment from 'moment';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Button, Table } from 'reactstrap';
 
 const TablePage = () => {
   const getData = () => {
@@ -11,6 +13,24 @@ const TablePage = () => {
     const keys = Object.keys(parsed);
     return keys.map(key => parsed[key]);
   };
+  const downloadXlsx = () => {
+    let data = getData();
+    const headers = data[0];
+    data[0] = Array(7);
+    data.unshift(Array(7));
+    data.unshift(Array(7));
+    data[1] = headers;
+    data = data.map(row => {
+      return [''].concat(row);
+    });
+    const filename = getFilename();
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+    const new_workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(new_workbook, worksheet, 'SheetJS');
+    XLSX.writeFile(new_workbook, filename);
+  };
+  const getFilename = () =>
+    `KMC Signins ${moment().format('ddd, MMMM Do')}.xlsx`;
   return (
     <>
       <CopyToClipboard text={window.location}>
@@ -18,14 +38,19 @@ const TablePage = () => {
           Copy this Url to Clipboard
         </Button>
       </CopyToClipboard>
+      <Button color="success" style={{ margin: '2em' }} onClick={downloadXlsx}>
+        Download .xlsx
+      </Button>
       <Table style={{ margin: '2em' }}>
         {getData().map((row, index) => {
           if (index === 0) {
             return (
               <thead>
-                {row.map(header => (
-                  <th>{header}</th>
-                ))}
+                <tr>
+                  {row.map(header => (
+                    <th>{header}</th>
+                  ))}
+                </tr>
               </thead>
             );
           } else {
