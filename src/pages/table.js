19f7d7182eaad317c1949f557059f5e3e2c8d9f2
Andrew Horn <arhorn@smcm.edu>
2019-07-22 01:13:15 -0500

    trying to correct gatsby error, second attempt

--- src/pages/table.js
+++ src/pages/table.js
@@ -7,9 +7,12 @@ import { Button, Table } from 'reactstrap';
 
 const TablePage = () => {
   const getData = () => {
-    const parsed = queryString.parse(window.location.search, {
-      arrayFormat: 'bracket'
-    });
+    const parsed = queryString.parse(
+      typeof window !== 'undefined' && window.location.search,
+      {
+        arrayFormat: 'bracket'
+      }
+    );
     const keys = Object.keys(parsed);
     return keys.map(key => parsed[key]);
   };
